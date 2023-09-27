1929bb2bccd53aae10d32bc9d4ef9dcf5a0977f4
Jainah Marie <jainahcartagena@gmail.com>
2023-09-27 14:33:40 +0800

    git commit -m "removed the getMethod"

--- main/index.js
+++ main/index.js
@@ -1,36 +1,23 @@
 const express = require('express');
 const app = express();
-const { initializeApp, credential } = require("firebase-admin");
+var admin = require("firebase-admin");
 const path = require('path');
 
 var serviceAccount = require(path.join(__dirname, 'svAcc', 'serviceAccount.json'));
 
 // Initialize Firebase Admin SDK (you need to set up your credentials)
-initializeApp({
-  credential: credential.cert(serviceAccount),
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://boxdots-ba3ca-default-rtdb.firebaseio.com"
 });
 
-// Define an endpoint to retrieve a specific child data
-app.get('/getUrl', (req, res) => {
-  const link = req.query.link; // Get the link from the request
-  
-  // Construct a query to find the urlResult with the given link
-  const urlsRef = admin.database().ref('urls');
-  urlsRef.orderByChild('url').equalTo(link).once('value')
-    .then(snapshot => {
-      if (snapshot.exists()) {
-        // Access the "urlResult" field for the app user
-        const linkKey = Object.keys(snapshot.val())[0]; // Get the link's key
-        const linkResult = snapshot.child(linkKey).child('urlResult').val();
-        res.json({ urlResult: linkResult });
-      } else {
-        res.status(404).json({ error: 'User not found' });
-      }
-    })
-    .catch(error => {
-      res.status(500).json({ error: 'Server error' });
-    });
+// As an admin, the app has access to read and write all data, regardless of Security Rules
+var db = admin.database();
+var ref = db.ref("urls");
+
+ref.on("value", function(snapshot) {
+  var data = snapshot.val();
+  console.log(data);
 });
 
 // Start the API server
