81393aff7f2ef8c5defa80b41e4ed4df32a1fce7
Jainah Marie <jainahcartagena@gmail.com>
2023-09-28 10:54:22 +0800

    git commit -m "api"

--- main/index.js
+++ main/index.js
@@ -21,6 +21,28 @@ ref.on("value", function(snapshot) {
   console.log(data);
 });
 
+// Define an endpoint to retrieve a specific child data
+app.get('/getUrl', (req, res) => {
+  const link = req.query.link; // Get the link from the request
+  
+  // Construct a query to find the urlResult with the given link
+  const urlsRef = admin.database().ref('urls');
+  urlsRef.orderByChild('url').equalTo(link).once('value')
+    .then(snapshot => {
+      if (snapshot.exists()) {
+        // Access the "urlResult" field for the app user
+        const linkKey = Object.keys(snapshot.val())[0]; // Get the link's key
+        const linkResult = snapshot.child(linkKey).child('urlResult').val();
+        res.json({ urlResult: linkResult });
+      } else {
+        res.status(404).json({ error: 'User not found' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'Server error' });
+    });
+});
+
 // Start the API server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
